fb1ef9a34644019f9d168d29cc934150abffa32c
Florin Baincescu <florin.baincescu@ness.com>
2019-09-12 16:16:39 +0300

    IOS-1383: Update pods - Buglife 2.8.4 -> 2.10.1

--- Pods/Buglife/Source/Views/GestureRecognizers/LIFEFreeformGestureRecognizer.h
+++ Pods/Buglife/Source/Views/GestureRecognizers/LIFEFreeformGestureRecognizer.h
@@ -1,6 +1,6 @@
 //
-//  LIFEToolButton.h
-//  Copyright (C) 2018 Buglife, Inc.
+//  UIContinuousForceTouchGestureRecognizer.h
+//  Copyright (C) 2019 Buglife, Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -14,23 +14,13 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-//
 
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSInteger, LIFEToolButtonType)
-{
-    LIFEToolButtonTypeArrow = 0,
-    LIFEToolButtonTypeLoupe = 1,
-    LIFEToolButtonTypeBlur = 2,
-    LIFEToolButtonTypeFreeform = 3
-};
+NS_ASSUME_NONNULL_BEGIN
 
-@interface LIFEToolButton : UIControl
-
-@property (nonnull, nonatomic) UIImageView *imageView;
-@property (nonnull, nonatomic) UILabel *titleView;
-
-- (void)setTintColor:(nonnull UIColor *)tintColor forState:(UIControlState)state;
+@interface LIFEFreeformGestureRecognizer : UIGestureRecognizer
 
 @end
+
+NS_ASSUME_NONNULL_END
